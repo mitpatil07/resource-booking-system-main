f189869d549b356949ad979415fef5e5fc90243a
Mitesh Patil <mitesh8767@gmail.com>
2025-08-02 21:34:06 +0530

    update

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from bookings.views import index
+# from bookings.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
