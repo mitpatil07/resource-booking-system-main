71db24e83898d7838970dbcba3979e0b862b852d
Mitesh Patil <mitesh8767@gmail.com>
2025-08-02 21:29:48 +0530

    update

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -157,7 +157,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://resource-booking-system-ct9e.onrender.com"
+    "https://resource-booking-system-rd80.onrender.com"
 ]
 
 from datetime import timedelta
